92147e43f3803384217c9853571a809766d2e6c2
Vatsal <rajvatsal2004@gmail.com>
2024-02-03 17:11:51 +0530

    feat(js): Add basic logic for the game
    It only has the logic to create players and check if the game is over or
    not. It currently doesn't take turns for player input and check for game
    over on it's own but you have the functions available the allow it to do
    so.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,79 @@
-console.log("hello world");
+const gameBoard = (function () {
+	const _emptyCell = "*";
+	let _board = [
+		[_emptyCell, _emptyCell, _emptyCell],
+		[_emptyCell, _emptyCell, _emptyCell],
+		[_emptyCell, _emptyCell, _emptyCell],
+	];
+
+	function render() {
+		for (let i of _board) {
+			console.log(i.join(" "));
+		}
+	}
+
+	function addInput(x, y) {
+		_board[x][y] = this.mark;
+	}
+
+	function isGameOver(plyr) {
+		for (let i = 0; i < _board.length; i++) {
+			let vert = 0,
+				horz = 0,
+				diag = 0;
+			for (let j = 0; j < _board.length; j++) {
+				if (_board[i][j] === plyr.mark) horz++;
+				if (_board[j][i] === plyr.mark) vert++;
+				if (_board[j][j] === plyr.mark) diag++;
+				else if (_board[i][_board[i].length - 1 - i] === plyr.mark) diag++;
+			}
+			if (diag === 3 || vert === 3 || horz === 3) return true;
+		}
+	}
+
+	return { render, addInput, isGameOver };
+})();
+
+const player = (function () {
+	function _createPlayer(name, mark) {
+		const { addInput: makeMove } = gameBoard;
+		return { name, mark, makeMove };
+	}
+
+	const ppl = [];
+	ppl.push(_createPlayer("vatsal", "X"));
+	ppl.push(_createPlayer("thanos", "O"));
+
+	return ppl;
+})();
+
+const renderArts = (function () {
+	let _arts = {
+		header: `
+			▄▄▄▄▄▪   ▄▄·     ▄▄▄▄▄ ▄▄▄·  ▄▄·     ▄▄▄▄▄      ▄▄▄ .
+			•██  ██ ▐█ ▌▪    •██  ▐█ ▀█ ▐█ ▌▪    •██  ▪     ▀▄.▀·
+			 ▐█.▪▐█·██ ▄▄     ▐█.▪▄█▀▀█ ██ ▄▄     ▐█.▪ ▄█▀▄ ▐▀▀▪▄
+			 ▐█▌·▐█▌▐███▌     ▐█▌·▐█ ▪▐▌▐███▌     ▐█▌·▐█▌.▐▌▐█▄▄▌
+			 ▀▀▀ ▀▀▀·▀▀▀      ▀▀▀  ▀  ▀ ·▀▀▀      ▀▀▀  ▀█▄▀▪ ▀▀▀ 
+		`,
+		end: `
+			 ▄▄ •  ▄▄▄· • ▌ ▄ ·. ▄▄▄ .           ▌ ▐·▄▄▄ .▄▄▄  
+			▐█ ▀ ▪▐█ ▀█ ·██ ▐███▪▀▄.▀·    ▪     ▪█·█▌▀▄.▀·▀▄ █·
+			▄█ ▀█▄▄█▀▀█ ▐█ ▌▐▌▐█·▐▀▀▪▄     ▄█▀▄ ▐█▐█•▐▀▀▪▄▐▀▀▄ 
+			▐█▄▪▐█▐█ ▪▐▌██ ██▌▐█▌▐█▄▄▌    ▐█▌.▐▌ ███ ▐█▄▄▌▐█•█▌
+			·▀▀▀▀  ▀  ▀ ▀▀  █▪▀▀▀ ▀▀▀      ▀█▄▀▪. ▀   ▀▀▀ .▀  ▀
+		`,
+	};
+	console.log(_arts.header);
+
+	const gameOver = () => console.log(_arts.end);
+
+	return { gameOver };
+})();
+
+player[1].makeMove(0, 2);
+player[1].makeMove(1, 1);
+player[1].makeMove(2, 0);
+gameBoard.render();
+if (gameBoard.isGameOver(player[0]) || gameBoard.isGameOver(player[1]))
+	renderArts.gameOver();
