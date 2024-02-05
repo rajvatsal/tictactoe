31b3032d0688d2987265f85ed6150f531cbb8d92
Vatsal <rajvatsal2004@gmail.com>
2024-02-05 15:57:38 +0530

    refactor(js): Encapsulate the entire game in a game object

--- js/script.js
+++ js/script.js
@@ -1,80 +1,81 @@
-const gameBoard = (function () {
-	const _emptyCell = "*";
-	let _board = [
-		[_emptyCell, _emptyCell, _emptyCell],
-		[_emptyCell, _emptyCell, _emptyCell],
-		[_emptyCell, _emptyCell, _emptyCell],
-	];
+const game = (function () {
+	const gameBoard = (function () {
+		const _emptyCell = "*";
+		let _board = [
+			[_emptyCell, _emptyCell, _emptyCell],
+			[_emptyCell, _emptyCell, _emptyCell],
+			[_emptyCell, _emptyCell, _emptyCell],
+		];
 
-	let _winner = "";
+		let _winner = "";
 
-	const getWinner = () => _winner;
+		const getWinner = () => _winner;
 
-	function render() {
-		for (let i of _board) {
-			console.log(i.join(" "));
-		}
-	}
-
-	function addInput(x, y) {
-		if (x > 2 || y > 2 || _board[x][y] !== "*") {
-			[x, y] = prompt(
-				"Invalid Input. Enter your x and y value again with a space.",
-			).split(" ");
-		}
-		_board[x][y] = this.mark;
-	}
-
-	function isDraw() {
-		for (let i of _board) {
-			for (let j of i) {
-				if (j === "*") return false;
+		function render() {
+			for (let i of _board) {
+				console.log(i.join(" "));
 			}
 		}
-		return true;
-	}
 
-	function isWinner(plyr) {
-		let diagR = 0;
-		for (let i = 0; i < _board.length; i++) {
-			let vert = 0,
-				horz = 0,
-				diagL = 0;
-
-			for (let j = 0; j < _board.length; j++) {
-				if (_board[i][j] === plyr.mark) horz++;
-				if (_board[j][i] === plyr.mark) vert++;
-				if (_board[j][j] === plyr.mark) diagL++;
+		function addInput(x, y) {
+			if (x > 2 || y > 2 || _board[x][y] !== "*") {
+				[x, y] = prompt(
+					"Invalid Input. Enter your x and y value again with a space.",
+				).split(" ");
 			}
-
-			if (_board[i][_board[i].length - 1 - i] === plyr.mark) diagR++;
-			if (diagR === 3 || vert === 3 || horz === 3 || diagL === 3) {
-				_winner = plyr.name;
-				return true;
-			}
+			_board[x][y] = this.mark;
 		}
-		return false;
-	}
 
-	return { render, addInput, isWinner, isDraw, getWinner };
-})();
+		function isDraw() {
+			for (let i of _board) {
+				for (let j of i) {
+					if (j === "*") return false;
+				}
+			}
+			return true;
+		}
 
-const player = (function () {
-	function _createPlayer(name, mark) {
-		const { addInput: makeMove } = gameBoard;
-		return { name, mark, makeMove };
-	}
+		function isWinner(plyr) {
+			let diagR = 0;
+			for (let i = 0; i < _board.length; i++) {
+				let vert = 0,
+					horz = 0,
+					diagL = 0;
 
-	const ppl = [];
-	ppl.push(_createPlayer("vatsal", "X"));
-	ppl.push(_createPlayer("thanos", "O"));
+				for (let j = 0; j < _board.length; j++) {
+					if (_board[i][j] === plyr.mark) horz++;
+					if (_board[j][i] === plyr.mark) vert++;
+					if (_board[j][j] === plyr.mark) diagL++;
+				}
 
-	return ppl;
-})();
+				if (_board[i][_board[i].length - 1 - i] === plyr.mark) diagR++;
+				if (diagR === 3 || vert === 3 || horz === 3 || diagL === 3) {
+					_winner = plyr.name;
+					return true;
+				}
+			}
+			return false;
+		}
 
-const renderArts = (function () {
-	let _arts = {
-		header: `
+		return { render, addInput, isWinner, isDraw, getWinner };
+	})();
+
+	const player = (function () {
+		function _createPlayer(name, mark) {
+			const { addInput: makeMove } = gameBoard;
+			return { name, mark, makeMove };
+		}
+
+		const ppl = [];
+		ppl.push(_createPlayer("vatsal", "X"));
+		ppl.push(_createPlayer("thanos", "O"));
+
+		return ppl;
+	})();
+
+	const renderArts = (function () {
+		let _arts = {
+			header: `
  ______                ______                     ______                  
 /\\__  _\\__            /\\__  _\\                   /\\__  _\\                 
 \\/_/\\ \\/\\_\\    ___    \\/_/\\ \\/    __      ___    \\/_/\\ \\/   ___      __   
@@ -84,7 +85,7 @@ const renderArts = (function () {
       \\/_/\\/_/\\/____/       \\/_/\\/__/\\/_/\\/____/       \\/_/\\/___/  \\/____/
 
 `,
-		winner: `
+			winner: `
             __                                 
  __  __  __/\\_\\    ___     ___      __   _ __  
 /\\ \\/\\ \\/\\ \\/\\ \\ /\' _ \`\\ /\' _ \`\\  /\'__\`\\/\\\`\'__\\
@@ -92,40 +93,42 @@ const renderArts = (function () {
  \\ \\___x___/\'\\ \\_\\ \\_\\ \\_\\ \\_\\ \\_\\ \\____\\\\ \\_\\ 
   \\/__//__/   \\/_/\\/_/\\/_/\\/_/\\/_/\\/____/ \\/_/ 
 `,
-		draw: `
+			draw: `
 ·▄▄▄▄  ▄▄▄   ▄▄▄· ▄▄▌ ▐ ▄▌
 ██▪ ██ ▀▄ █·▐█ ▀█ ██· █▌▐█
 ▐█· ▐█▌▐▀▀▄ ▄█▀▀█ ██▪▐█▐▐▌
 ██. ██ ▐█•█▌▐█ ▪▐▌▐█▌██▐█▌
 ▀▀▀▀▀• .▀  ▀ ▀  ▀  ▀▀▀▀ ▀▪
 		`,
-	};
-	console.log(_arts.header);
+		};
+		console.log(_arts.header);
 
-	const gameOver = (val) => {
-		if (val) console.log(_arts.winner, val);
-		else console.log(_arts.draw);
-	};
+		const gameOver = (val) => {
+			if (val) console.log(_arts.winner, val);
+			else console.log(_arts.draw);
+		};
 
-	return { gameOver };
-})();
+		return { gameOver };
+	})();
 
-const game = (function () {
-	const { isWinner, render, isDraw, getWinner } = gameBoard;
-	while (true) {
-		player[0].makeMove(
-			prompt("Player one X-coordinate:"),
-			prompt("Player one Y-coordinate:"),
-		);
-		render();
-		if (isWinner(player[0]) || isDraw()) break;
-		player[1].makeMove(
-			prompt("Player two X-coordinate:"),
-			prompt("Player two Y-coordinate:"),
-		);
-		render();
-		if (isWinner(player[1]) || isDraw()) break;
+	function init() {
+		const { isWinner, render, isDraw, getWinner } = gameBoard;
+		while (true) {
+			player[0].makeMove(
+				prompt("Player one X-coordinate:"),
+				prompt("Player one Y-coordinate:"),
+			);
+			render();
+			if (isWinner(player[0]) || isDraw()) break;
+			player[1].makeMove(
+				prompt("Player two X-coordinate:"),
+				prompt("Player two Y-coordinate:"),
+			);
+			render();
+			if (isWinner(player[1]) || isDraw()) break;
+		}
+
+		renderArts.gameOver(getWinner());
 	}
-
-	renderArts.gameOver(getWinner());
+	return { init };
 })();
