eac3b58c70bc101a3c50a851ae7534e4e60e17ea
Vatsal <rajvatsal2004@gmail.com>
2024-02-07 21:04:31 +0530

    feat(GameController): Add playRound method that controlles flow of the game

--- js/script.js
+++ js/script.js
@@ -1,89 +1,88 @@
-const game = (function () {
-	const Gameboard = (function () {
-		const _emptyCell = "*";
-		const _rows = 3;
-		const _columns = 3;
-		const _board = [];
+const Gameboard = (function () {
+	const _emptyCell = "*";
+	const _rows = 3;
+	const _columns = 3;
+	const _board = [];
 
-		//Create a 3X3 board
+	//Create a 3X3 board
+	for (let i = 0; i < _rows; i++) {
+		_board[i] = [];
+		for (let j = 0; j < _columns; j++) {
+			_board[i].push(_emptyCell);
+		}
+	}
+
+	let _winner = "";
+
+	const getWinner = () => _winner;
+
+	function render() {
+		for (let i of _board) {
+			console.log(i.join(" "));
+		}
+	}
+
+	function placeMark(plyr, x, y) {
+		if (x > _columns - 1 || y > _rows - 1 || _board[x][y] !== _emptyCell) {
+			[x, y] = prompt(
+				"Invalid Input. Enter your x and y value again with a space.",
+			).split(" ");
+		}
+		if (!plyr.validPlayer) return;
+		_board[x][y] = plyr.mark;
+	}
+
+	function isDraw() {
+		for (let i of _board) {
+			for (let j of i) {
+				if (j === _emptyCell) return false;
+			}
+		}
+		return true;
+	}
+
+	function isWinner() {
+		let diagR = 0;
 		for (let i = 0; i < _rows; i++) {
-			_board[i] = [];
+			let vert = 0,
+				horz = 0,
+				diagL = 0;
+
 			for (let j = 0; j < _columns; j++) {
-				_board[i].push(_emptyCell);
+				if (_board[i][j] === this.mark) horz++;
+				if (_board[j][i] === this.mark) vert++;
+				if (_board[j][j] === this.mark) diagL++;
+			}
+
+			if (_board[i][_columns - 1 - i] === this.mark) diagR++;
+			if (
+				diagR === _rows ||
+				vert === _columns ||
+				horz === _rows ||
+				diagL === _rows
+			) {
+				_winner = this.name;
+				return true;
 			}
 		}
+		return false;
+	}
 
-		let _winner = "";
+	return { render, placeMark, isWinner, isDraw, getWinner };
+})();
 
-		const getWinner = () => _winner;
+const Player = (function () {
+	function create(values) {
+		const instance = Object.create(this);
+		return Object.assign(instance, values);
+	}
 
-		function render() {
-			for (let i of _board) {
-				console.log(i.join(" "));
-			}
-		}
+	return { create };
+})();
 
-		function placeMark(plyr, [x, y]) {
-			if (x > _columns - 1 || y > _rows - 1 || _board[x][y] !== _emptyCell) {
-				[x, y] = prompt(
-					"Invalid Input. Enter your x and y value again with a space.",
-				).split(" ");
-			}
-			if (!plyr.validPlayer) return;
-			_board[x][y] = plyr.mark;
-		}
-
-		function isDraw() {
-			for (let i of _board) {
-				for (let j of i) {
-					if (j === _emptyCell) return false;
-				}
-			}
-			return true;
-		}
-
-		function isWinner() {
-			let diagR = 0;
-			for (let i = 0; i < _rows; i++) {
-				let vert = 0,
-					horz = 0,
-					diagL = 0;
-
-				for (let j = 0; j < _columns; j++) {
-					if (_board[i][j] === this.mark) horz++;
-					if (_board[j][i] === this.mark) vert++;
-					if (_board[j][j] === this.mark) diagL++;
-				}
-
-				if (_board[i][_columns - 1 - i] === this.mark) diagR++;
-				if (
-					diagR === _rows ||
-					vert === _columns ||
-					horz === _rows ||
-					diagL === _rows
-				) {
-					_winner = this.name;
-					return true;
-				}
-			}
-			return false;
-		}
-
-		return { render, placeMark, isWinner, isDraw, getWinner };
-	})();
-
-	const Player = (function () {
-		function create(values) {
-			const instance = Object.create(this);
-			return Object.assign(instance, values);
-		}
-
-		return { create };
-	})();
-
-	const renderArt = (function () {
-		let _arts = {
-			header: `
+const renderArt = (function () {
+	let _arts = {
+		header: `
  ______                ______                     ______                  
 /\\__  _\\__            /\\__  _\\                   /\\__  _\\                 
 \\/_/\\ \\/\\_\\    ___    \\/_/\\ \\/    __      ___    \\/_/\\ \\/   ___      __   
@@ -93,7 +92,7 @@ const game = (function () {
       \\/_/\\/_/\\/____/       \\/_/\\/__/\\/_/\\/____/       \\/_/\\/___/  \\/____/
 
 `,
-			winner: `
+		winner: `
             __                                 
  __  __  __/\\_\\    ___     ___      __   _ __  
 /\\ \\/\\ \\/\\ \\/\\ \\ /\' _ \`\\ /\' _ \`\\  /\'__\`\\/\\\`\'__\\
@@ -101,53 +100,44 @@ const game = (function () {
  \\ \\___x___/\'\\ \\_\\ \\_\\ \\_\\ \\_\\ \\_\\ \\____\\\\ \\_\\ 
   \\/__//__/   \\/_/\\/_/\\/_/\\/_/\\/_/\\/____/ \\/_/ 
 `,
-			draw: `
+		draw: `
 ·▄▄▄▄  ▄▄▄   ▄▄▄· ▄▄▌ ▐ ▄▌
 ██▪ ██ ▀▄ █·▐█ ▀█ ██· █▌▐█
 ▐█· ▐█▌▐▀▀▄ ▄█▀▀█ ██▪▐█▐▐▌
 ██. ██ ▐█•█▌▐█ ▪▐▌▐█▌██▐█▌
 ▀▀▀▀▀• .▀  ▀ ▀  ▀  ▀▀▀▀ ▀▪
 		`,
-		};
-		console.log(_arts.header);
+	};
+	console.log(_arts.header);
 
-		const gameOver = (val) => {
-			if (val) console.log(_arts.winner, val);
-			else console.log(_arts.draw);
-		};
+	const gameOver = (val) => {
+		if (val) console.log(_arts.winner, val);
+		else console.log(_arts.draw);
+	};
 
-		return { gameOver };
-	})();
+	return { gameOver };
+})();
 
-	function init() {
-		const { render, isDraw, getWinner, placeMark, isWinner } = Gameboard;
+const GameController = (function () {
+	const { render, isDraw, getWinner, placeMark, isWinner } = Gameboard;
 
-		const players = [
-			Player.create({ name: "vatsal", mark: "X", validPlayer: true }),
-			Player.create({ name: "thanos", mark: "O", validPlayer: true }),
-		];
+	const players = [
+		Player.create({ name: "vatsal", mark: "X", validPlayer: true }),
+		Player.create({ name: "thanos", mark: "O", validPlayer: true }),
+	];
+	let activePlayer = players[0];
 
-		function getCoord() {
-			return [
-				prompt("Player one X-coordinate:"),
-				prompt("Player one Y-coordinate:"),
-			];
-		}
+	const switchPlayer = () =>
+		activePlayer === players[0]
+			? (activePlayer = players[1])
+			: (activePlayer = players[0]);
 
-		while (true) {
-			placeMark(players[0], getCoord());
-			render();
-			if (isWinner.call(players[0]) || isDraw()) break;
-			placeMark(players[1], getCoord());
-			render();
-			if (isWinner.call(players[1]) || isDraw()) break;
-		}
-
-		renderArt.gameOver(getWinner());
+	function playRound(x, y) {
+		placeMark(activePlayer, x, y);
+		render();
+		if (isWinner.call(activePlayer) || isDraw()) renderArt.gameOver(getWinner);
+		switchPlayer();
 	}
 
-	return {
-		init,
-		render: Gameboard.render,
-	};
+	return { playRound };
 })();
